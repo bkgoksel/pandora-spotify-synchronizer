494401778465d53b55e2b01f97f7cc547b1ba8e1
Kerem Goksel <bkgoksel@gmail.com>
2015-04-13 15:13:59 -0700

    More refactoring, now adds songs to a playlist called 'Liked from Pandora' but infinite loops if such a playlist doesn't exist

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+var extensionId = "jefocpemkfdpaofkldjigkepjfpgombg";
+var spotifyAPIClientId = "6a7d3885ccb34286a48690fd600ee2af";
+var spotifyAPIClientSecret = "56e23715dab04e0a96c852e879a605df";
+var playlistName = 'Liked from Pandora';
+
 // listen for a song being liked on the page.
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.type) {
@@ -10,13 +15,15 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 //Search for the song on Spotify and find its Spotify URI if the song exists.
 function getSpotifySongURI(songData) {
-    alert(songData.songName);
-    var searchURL = "https://api.spotify.com/v1/search" + "?q=track:" + encodeURIComponent(songData.songName) + "+album:" + encodeURIComponent(songData.albumName) + "+artist:" + encodeURIComponent(songData.artistName) + "&type=track"; 
     $.ajax({
-        url: searchURL,
+        url: "https://api.spotify.com/v1/search?query=track:" + encodeURIComponent(songData.songName) + "+album:" + encodeURIComponent(songData.albumName) + "+artist:" + encodeURIComponent(songData.artistName) + "&type=track",
         success: function(songSearchResultJSON) {
-            var songURI = songSearchResultJSON.tracks.items[0].uri;
-            authenticate(songURI);
+            if (songSearchResultJSON.tracks.items.length < 1) {
+                alert("Song not found on Spotify!");
+            } else {
+                var songURI = songSearchResultJSON.tracks.items[0].uri;
+                authenticate(songURI);
+            }
         },
         error: function(data) {
             alert("Song not found on Spotify");
@@ -28,7 +35,7 @@ function getSpotifySongURI(songData) {
 function authenticate(songURI) {
     chrome.identity.launchWebAuthFlow(
         {
-            'url': 'https://accounts.spotify.com/authorize?client_id=6a7d3885ccb34286a48690fd600ee2af&response_type=code&redirect_uri=https://dpinhkoooicmkogpadlafpoljkihgmmg.chromiumapp.org/callback&scope=playlist-modify-public%20playlist-modify-private', 
+            'url': 'https://accounts.spotify.com/authorize?client_id=' + spotifyAPIClientId + '&response_type=code&redirect_uri=https://' + extensionId + '.chromiumapp.org/callback&scope=playlist-modify-public%20playlist-modify-private', 
             'interactive': true
         },
         function(redirect_url) { 
@@ -39,9 +46,10 @@ function authenticate(songURI) {
                 data: { 
                     grant_type: "authorization_code", 
                     code: code, 
-                    redirect_uri: "https://dpinhkoooicmkogpadlafpoljkihgmmg.chromiumapp.org/callback",
-                    client_id: "6a7d3885ccb34286a48690fd600ee2af",
-                    client_secret: "56e23715dab04e0a96c852e879a605df" },
+                    redirect_uri: "https://" + extensionId + ".chromiumapp.org/callback",
+                    client_id: spotifyAPIClientId,
+                    client_secret:  spotifyAPIClientSecret 
+                },
                 success: function(authData) {
                     extractUserTokens(authData, songURI);
                 },
@@ -61,7 +69,7 @@ function extractUserTokens(authJSON, songURI) {
             'Authorization': 'Bearer ' + authJSON.access_token
         },
         success: function(user) {
-            getPlaylist(authJSON.access_token, user, songURI);
+            getPlaylists(authJSON.access_token, user, songURI, '0');
         },
         error: function(response) {
             alert("User not found!");
@@ -70,12 +78,16 @@ function extractUserTokens(authJSON, songURI) {
 }
 
 //Get a list of the user's playlists with the given access token and user JSON object
-function getPlaylist(accessToken, userObj, songURI) {
+function getPlaylists(accessToken, userObj, songURI, offset) {
     var userId = userObj.id;
     $.ajax({
         url: 'https://api.spotify.com/v1/users/' + userId + '/playlists',
         headers: {
             'Authorization': 'Bearer ' + accessToken
+        },
+        data: {
+            'limit': '50',
+            'offset': offset
         },
         success: function(playlists) {
             addTrack(accessToken, playlists, userId, songURI);
@@ -88,24 +100,43 @@ function getPlaylist(accessToken, userObj, songURI) {
 
 // Find the "Starred" playlist from the list of playlists and add the song with the given URI to the playlist
 function addTrack(accessToken, playlistsObj, userId, songURI) {
-    var playlistId = playlistsObj.items[1].id;    
-    for (var playlist in playlistsObj.items) {
-        if (playlist.name === 'Starred') {
+    var playlistId = -1;    
+    playlistsObj.items.forEach( function(playlist) {
+        if (playlist.name === playlistName) {
             playlistId = playlist.id;
             alert(playlist.name);
         }
+    });
+    if (playlistId === -1) {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/users/' + userId + '/playlists',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            data: {
+                'limit': '50',
+                'offset': (playlistsObj.limit + playlistsObj.offset).toString()
+            },
+            success: function(playlists) {
+                addTrack(accessToken, playlists, userId, songURI);
+            },
+            error: function(response) {
+                alert("Can't access playlists!");
+            }
+        })
+    } else {
+        $.ajax({
+            method: "POST",
+            url: 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks?uris=' + encodeURIComponent(songURI),
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+            },
+            success: function(response) {
+                alert('Song added to playlist successfully!');
+            },
+            error: function(response) {
+                alert("Song couldn't be added successfully!");
+            }
+        })
     }
-    $.ajax({
-        method: "POST",
-        url: 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks?uris=' + encodeURIComponent(songURI),
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        },
-        success: function(response) {
-            alert('Song added to playlist successfully!');
-        },
-        error: function(response) {
-            alert("Song couldn't be added successfully!");
-        }
-    })
 }
