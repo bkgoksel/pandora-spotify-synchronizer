a73d9723e873d664c78b895d3f7afe605f2aa9f7
Kerem Goksel <bkgoksel@gmail.com>
2015-04-13 13:45:15 -0700

    Commenting and searching for Starred playlist

--- background.js
+++ background.js
@@ -8,23 +8,24 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     return true;
 });
 
+//Search for the song on Spotify and find its Spotify URI if the song exists.
 function getSpotifySongURI(songData) {
     alert(songData.songName);
     var searchURL = "https://api.spotify.com/v1/search" + "?q=track:" + encodeURIComponent(songData.songName) + "+album:" + encodeURIComponent(songData.albumName) + "+artist:" + encodeURIComponent(songData.artistName) + "&type=track"; 
     $.ajax({
         url: searchURL,
-        success: function(data) {
-            extractIdfromJSON(data);
+        success: function(songSearchResultJSON) {
+            var songURI = songSearchResultJSON.tracks.items[0].uri;
+            authenticate(songURI);
+        },
+        error: function(data) {
+            alert("Song not found on Spotify");
         }
     })
 }
 
-function extractIdfromJSON(songJSON) {
-    var songId = songJSON.tracks.items[0].uri;
-    authenticate(songId);
-}
-
-function authenticate(songId) {
+//Authenticate the Spotify user and on redirect, call the function to extract the user information
+function authenticate(songURI) {
     chrome.identity.launchWebAuthFlow(
         {
             'url': 'https://accounts.spotify.com/authorize?client_id=6a7d3885ccb34286a48690fd600ee2af&response_type=code&redirect_uri=https://dpinhkoooicmkogpadlafpoljkihgmmg.chromiumapp.org/callback&scope=playlist-modify-public%20playlist-modify-private', 
@@ -42,13 +43,17 @@ function authenticate(songId) {
                     client_id: "6a7d3885ccb34286a48690fd600ee2af",
                     client_secret: "56e23715dab04e0a96c852e879a605df" },
                 success: function(authData) {
-                    extractUserTokens(authData, songId);
+                    extractUserTokens(authData, songURI);
+                },
+                error: function(response) {
+                    alert("Authentication failed!");
                 }
             })
     });
 }
 
-function extractUserTokens(authJSON, songId) {
+//Extract the access token and user JSON object given the auth response
+function extractUserTokens(authJSON, songURI) {
     alert(authJSON.access_token);
     $.ajax({
         url: 'https://api.spotify.com/v1/me',
@@ -56,12 +61,16 @@ function extractUserTokens(authJSON, songId) {
             'Authorization': 'Bearer ' + authJSON.access_token
         },
         success: function(user) {
-            getPlaylist(authJSON.access_token, user, songId);
+            getPlaylist(authJSON.access_token, user, songURI);
+        },
+        error: function(response) {
+            alert("User not found!");
         }
     })
 }
 
-function getPlaylist(accessToken, userObj, songId) {
+//Get a list of the user's playlists with the given access token and user JSON object
+function getPlaylist(accessToken, userObj, songURI) {
     var userId = userObj.id;
     $.ajax({
         url: 'https://api.spotify.com/v1/users/' + userId + '/playlists',
@@ -69,38 +78,34 @@ function getPlaylist(accessToken, userObj, songId) {
             'Authorization': 'Bearer ' + accessToken
         },
         success: function(playlists) {
-            addTrack(accessToken, playlists, userId, songId);
+            addTrack(accessToken, playlists, userId, songURI);
+        },
+        error: function(response) {
+            alert("Can't access playlists!");
         }
     })
 }
 
-function addTrack(accessToken, playlistsObj, userId, songId) {
-    var playlistId = playlistsObj.items[1].id;
-    alert(playlistsObj.items[1].name);
+// Find the "Starred" playlist from the list of playlists and add the song with the given URI to the playlist
+function addTrack(accessToken, playlistsObj, userId, songURI) {
+    var playlistId = playlistsObj.items[1].id;    
+    for (var playlist in playlistsObj.items) {
+        if (playlist.name === 'Starred') {
+            playlistId = playlist.id;
+            alert(playlist.name);
+        }
+    }
     $.ajax({
         method: "POST",
-        url: 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks?uris=' + encodeURIComponent(songId),
+        url: 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks?uris=' + encodeURIComponent(songURI),
         headers: {
             'Authorization': 'Bearer ' + accessToken
         },
         success: function(response) {
-            alert('success!');
+            alert('Song added to playlist successfully!');
         },
         error: function(response) {
-            alert(response.items[0].message);
+            alert("Song couldn't be added successfully!");
         }
     })
-}
-
-function getObjects(obj, key, val) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getObjects(obj[i], key, val));
-        } else if (i == key && obj[key] == val) {
-            objects.push(obj);
-        }
-    }
-    return objects;
 }
